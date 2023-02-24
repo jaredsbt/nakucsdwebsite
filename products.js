5f99da61dc0d928f90e851099113f950af5ce720
jaredsbt <jaredsbt@gmail.com>
2023-02-23 23:19:03 -0800

    New JS file for selling products

---
+++ products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: 0,
+        name: "Esquites",
+        price: 6.00,
+        description: 
+            "Off the cob corn, served with your choice of toppings",
+    },
+
+    {
+        id: 1,
+        name: "Rueditas",
+        price: 5.00,
+        description: "Fried flour pinwheels, garnished with your choice of toppings",
+    },
+
+    {
+        id: 2,
+        name: "Nachos",
+        price: 3.00,
+        description: "Served with cheese and optional jalapenos",
+    },
+
+    {
+        id: 3,
+        name: "Hot Cheeto's with cheese",
+        price: 5.00,
+        
+    }, 
+
+    {
+        id: 4,
+        name: "Aguas Frescas Horachata",
+        price: 5.00,
+        description: "Rimmed with Nutella or Lechera and Cinnanom",
+    },
+
+    {
+        id: 5,
+        name: "Lime with Cucumber and Chia Seeds",
+        price: 5.00,
+        description: "Rimmed with Tajin and Chamoy",
+    }
+]
