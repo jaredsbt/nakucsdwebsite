28e3ed705b791f7e2066d45eec890d1f6f896d4d
jaredsbt <jaredsbt@gmail.com>
2023-02-25 00:26:12 -0800

    added inner html code under forEach function

--- renderProducts.js
+++ renderProducts.js
@@ -1,2 +1,27 @@
 // Elements
 const productsE1 = document.querySelector(".products");
+
+// Render Products
+function renderProducts() {
+    products.forEach( (product) => {
+        productsE1.innerHTML += `
+            <div class = "item">
+                <div class = "item-container">
+                    <div class = "item-img"></div>
+                    <div class = "desc">
+                        <h2>Esquites</h2>
+                        <h3><small>$</small>6.00</h3>
+                        <p>Off the cobb corn,  served with your choie toppings</p>
+                    </div>
+                    <div class = "add-to-wishlist">
+                        <img src = "">
+                    </div>
+                    <div class = "add-to-cart">
+                        <img src = "">
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+renderProducts();
