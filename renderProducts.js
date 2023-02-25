bda4c17cddb23599c5529d045877a39c17322636
jaredsbt <jaredsbt@gmail.com>
2023-02-25 00:32:03 -0800

    scripted render product file to html file

--- renderProducts.js
+++ renderProducts.js
@@ -9,9 +9,9 @@ function renderProducts() {
                 <div class = "item-container">
                     <div class = "item-img"></div>
                     <div class = "desc">
-                        <h2>Esquites</h2>
-                        <h3><small>$</small>6.00</h3>
-                        <p>Off the cobb corn,  served with your choie toppings</p>
+                        <h2>${product.name}</h2>
+                        <h3><small>$</small>${product.price}</h3>
+                        <p>${product.description}</p>
                     </div>
                     <div class = "add-to-wishlist">
                         <img src = "">
